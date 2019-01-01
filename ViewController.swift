1eebb652730febd2b75b4b786bf2e39c9f2842c2
Edgysan <43236401+Edgysan@users.noreply.github.com>
2019-01-01 17:40:18 +0100

    Add files via upload

---
+++ ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TokkiTest
+//
+//  Created by Test on 01/01/2019.
+//  Copyright © 2019 Test. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
